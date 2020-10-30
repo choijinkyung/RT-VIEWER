462497c4ef0c95b73cc3c92ec70d809106ce6c4f
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-10-30 15:26:03 +0900

    Cleanup by Folder

--- src/main/Loader/dragAndDrop.js
+++ src/main/Loader/dragAndDrop.js
@@ -1,7 +1,8 @@
 import * as cornerstoneWadoImageLoader from "cornerstone-wado-image-loader"
-import {doseFile} from "../isodose";
+import {doseFile} from "../RT_DOSE/isodose";
+
 // this function gets called once the user drops the file onto the div
-//for drag and drop
+//for drag and drop import doseFile
 function handleFileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
@@ -9,17 +10,15 @@ function handleFileSelect(evt) {
     // Get the FileList object that contains the list of files that were dropped
     let files = evt.dataTransfer.files;
 
-    // this UI is only built for a single file so just dump the first one
-    //structFile(files[0]);
     doseFile(files[0]);
     const imageId = cornerstoneWadoImageLoader.wadouri.fileManager.add(files[0]);
-    //doseData(imageId);
 }
 
 //this function manage drag event
 function handleDragOver(evt) {
     evt.stopPropagation();
     evt.preventDefault();
+
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
 
