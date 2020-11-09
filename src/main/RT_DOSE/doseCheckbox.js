f2f5aaa15efe3e10f40509115cb8d0d8862a6feb
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-09 13:34:09 +0900

    fix isodose percent

--- src/main/RT_DOSE/doseCheckbox.js
+++ src/main/RT_DOSE/doseCheckbox.js
@@ -3,13 +3,13 @@ import {getCheckValue} from "./isodose";
 
 function Dose_Checkbox(dosemax) {
     let level = [];
-
+    let pres = 40 * 100; //cGy
     level[0] = parseInt(dosemax);
-    level[1] = 100;
-    level[2] = 90;
-    level[3] = 70;
-    level[4] = 50;
-    level[5] = 30;
+    level[1] = pres;
+    level[2] = pres * 0.8;
+    level[3] = pres * 0.6;
+    level[4] = pres * 0.4;
+    level[5] = pres * 0.2;
 
     level.forEach(function (n) {
         let ul = document.getElementById('ul2');
@@ -22,15 +22,15 @@ function Dose_Checkbox(dosemax) {
 
         li.append(checkbox);
 
-        let text = n + '% /' + (n * 40) + 'cGy';
+        let text = parseInt(n / pres * 100) + '% / ' + n + 'cGy';
         li.append(document.createTextNode(text));
         ul.append(li);
     });
 }
 
-function Dose_checkEvent(){
+function Dose_checkEvent() {
     /*Event Listener*/
-    $(document).ready(function(){
+    $(document).ready(function () {
         let dose = document.getElementsByName("dose");
         if (dose[0].addEventListener) {
             for (let i = 0; i < dose.length; i++) {
@@ -44,22 +44,25 @@ function Dose_checkEvent(){
     })
 
 }
+
 let information = {
     Dose: []
 }
-function addDoseSet(evt) {
-    let checkVal_check;
 
-    if (evt.target.checked == true) {
+function addDoseSet(evt) {
+    let checkVal_check = [];
+
+    if (evt.target.checked === true) {
         information.Dose.push(evt.target.value);
         getCheckValue(information.Dose);
-    }else {
+    } else {
         let index = information.Dose.indexOf(evt.target.value);
-        if (index !== -1){
+        if (index !== -1) {
             information.Dose.splice(index, 1);
         }
         checkVal_check = evt.target.value;
         //getCheckValue(checkVal_check);
     }
 }
-export {Dose_Checkbox,Dose_checkEvent};
+
+export {Dose_Checkbox, Dose_checkEvent};
