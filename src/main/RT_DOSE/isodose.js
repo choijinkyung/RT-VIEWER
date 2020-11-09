073c07abc9a82cfee375efab980755500d99d3aa
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-09 16:29:06 +0900

    add dose CT align

--- src/main/RT_DOSE/isodose.js
+++ src/main/RT_DOSE/isodose.js
@@ -2,7 +2,6 @@ import * as cornerstone from "cornerstone-core";
 import * as cornerstoneWadoImageLoader from "cornerstone-wado-image-loader"
 import dicomParser from "dicom-parser";
 import {dose_pixel_Data_parse} from "./dosePixelDataParse";
-import {Dose_checkEvent} from "./doseCheckbox";
 
 let dataSet;
 let Rows, Columns, Number_of_Frames;
@@ -42,18 +41,30 @@ function doseFile(file) {
     return dataSet;
 }
 
+let imgOriArr = [];
+let imgPosArr = [];
+let pixelSpaceArr = [];
+
 function doseData(imageId, dataSet) {
-    let dose_pixelSpacing = 0;
+
     let img = 0;
 
     cornerstone.loadImage(imageId).then(function (image) {
         if (image.data.string('x00080016') === '1.2.840.10008.5.1.4.1.1.481.2') {
+            let dose_pixelSpacing = 0;
+            pixelSpaceArr = [];
 
             dose_pixelSpacing = image.data.string('x00280030');
-            dose_pixelSpacing = dose_pixelSpacing.split('\\');
-            dose_pixelSpacing = parseFloat(dose_pixelSpacing[0]);
+            dose_pixelSpacing = dose_pixelSpacing.toString();
+            pixelSpaceArr = dose_pixelSpacing.split("\\");
 
-            doseAlign(image);
+            let imgPos = image.data.string('x00200032');
+            imgPosArr = imgPos.split("\\");
+
+            let imgOri = image.data.string('x00200037');
+            imgOri = imgOri.toString();
+            imgOriArr = imgOri.split("\\");
+
 
             dose_pixel_Data_parse(image, dataSet);
         }
@@ -62,63 +73,6 @@ function doseData(imageId, dataSet) {
 
 }
 
-function doseAlign(image) {
-    let modality = image.data.string('x00080060');
-    let SOP_UID = image.data.string('x00080016');
-    let Sx, Sy, Sz;
-    if (modality === ('CT') || SOP_UID === '1.2.840.10008.5.1.4.1.1.481.2' || modality === 'RTDOSE') {
-        let imgPos = image.data.string('x00200032');
-        let imgPosArr = imgPos.split("\\");
-
-        Sx = (parseFloat(imgPosArr[0]) * 10) / 10;
-        Sy = (parseFloat(imgPosArr[1]) * 10) / 10;
-        Sz = (parseFloat(imgPosArr[2]) * 10) / 10;
-
-        let imgOri = image.data.string('x00200037');
-        imgOri = imgOri.toString();
-        let imgOriArr = imgOri.split("\\");
-
-        let Xx = (parseFloat(imgOriArr[0]) * 10) / 10;
-        let Xy = (parseFloat(imgOriArr[1]) * 10) / 10;
-        let Xz = (parseFloat(imgOriArr[2]) * 10) / 10;
-        let Yx = (parseFloat(imgOriArr[3]) * 10) / 10;
-        let Yy = (parseFloat(imgOriArr[4]) * 10) / 10;
-        let Yz = (parseFloat(imgOriArr[5]) * 10) / 10;
-
-        let pixelSpace = image.data.string('x00280030');
-        pixelSpace = pixelSpace.toString();
-        let pixelSpaceArr = pixelSpace.split("\\");
-
-        let Di = parseFloat((pixelSpaceArr[0]) * 10) / 10;
-        let Dj = parseFloat((pixelSpaceArr[1]) * 10) / 10;
-
-        let el = document.getElementById('dicomImage');
-
-        el.addEventListener('mousemove', function (event) {
-            const pixelCoords = cornerstone.pageToPixel(el, event.pageX, event.pageY);
-
-            let Px = (Xx * Di * pixelCoords.x) + (Yx * Dj * pixelCoords.y) + Sx;
-            let Py = (Xy * Di * pixelCoords.x) + (Yy * Dj * pixelCoords.y) + Sy;
-            let Pz = (Xz * Di * pixelCoords.x) + (Yz * Dj * pixelCoords.y) + Sz;
-
-            //document.getElementById('voxelCoords').textContent = "Px = " + Px + ", Py = " + Py + ", Pz = " + Pz;
-        });
-
-
-        el.addEventListener('dblclick', function (event) {
-            const pixelCoords = cornerstone.pageToPixel(el, event.pageX, event.pageY);
-
-            let Px = (Xx * Di * pixelCoords.x) + (Yx * Dj * pixelCoords.y) + Sx;
-            let Py = (Xy * Di * pixelCoords.x) + (Yy * Dj * pixelCoords.y) + Sy;
-            let Pz = (Xz * Di * pixelCoords.x) + (Yz * Dj * pixelCoords.y) + Sz;
-
-            // document.getElementById('voxelValue').textContent = "Px = " + Px + ", Py = " + Py + ", Pz = " + Pz;
-        });
-    } else {
-        alert('NOT CT IMAGES')
-    }
-    return [Sz];
-}
 
 let checkVal_check_dose = [];
 
@@ -133,28 +87,98 @@ function checkAndDraw(dose_value) {
     color[2] = '#ffff00';
     color[3] = '#44ff00';
     color[4] = '#ffcc33';
-    color[5] = '#2100ff'
+    color[5] = '#2100ff';
 
     for (let i = 0; i < checkVal_check_dose.length; i++) {
-        if (checkVal_check_dose[i] === '121') {
-            drawDose(dose_value, checkVal_check_dose[i], color[0]);
-        } else if (checkVal_check_dose[i] === '100') {
-            drawDose(dose_value, checkVal_check_dose[i], color[1]);
-        } else if (checkVal_check_dose[i] === '90') {
-            drawDose(dose_value, checkVal_check_dose[i], color[2]);
-        } else if (checkVal_check_dose[i] === '70') {
-            drawDose(dose_value, checkVal_check_dose[i], color[3]);
-        } else if (checkVal_check_dose[i] === '50') {
-            drawDose(dose_value, checkVal_check_dose[i], color[4]);
-        } else if (checkVal_check_dose[i] === '30') {
-            drawDose(dose_value, checkVal_check_dose[i], color[5]);
+        checkVal_check_dose[i] = parseInt(checkVal_check_dose[i]);
+    }
+    for (let i = 0; i < checkVal_check_dose.length; i++) {
+        if (checkVal_check_dose[i] === 4677) {
+            checkXY(dose_value, checkVal_check_dose[i], color[0]);
+        } else if (checkVal_check_dose[i] === 4000) {
+            checkXY(dose_value, checkVal_check_dose[i], color[1]);
+        } else if (checkVal_check_dose[i] === 3200) {
+            checkXY(dose_value, checkVal_check_dose[i], color[2]);
+        } else if (checkVal_check_dose[i] === 2400) {
+            checkXY(dose_value, checkVal_check_dose[i], color[3]);
+        } else if (checkVal_check_dose[i] === 1600) {
+            checkXY(dose_value, checkVal_check_dose[i], color[4]);
+        } else if (checkVal_check_dose[i] === 800) {
+            checkXY(dose_value, checkVal_check_dose[i], color[5]);
         }
     }
+
 }
 
-function drawDose(dose_value, checkVal_check, color) {
+let canvas_obj = [];
+
+function checkXY(dose_value, checkVal_check, color) {
+    let Vi = [], Vj = [];
+    let cnt = 0;
+
+    for (let y = 0; y < Columns; y++) {
+        for (let x = 0; x < Rows; x++) {
+            if (dose_value[y][x] < checkVal_check) {
+                Vi[cnt] = x;
+                Vj[cnt] = y;
+                cnt++;
+            }
+        }
+    }
+    doseAlign(Vi, Vj, color);
+}
+
+function doseAlign(Vi, Vj, color) {
+    let Sx = (parseFloat(imgPosArr[0]) * 10) / 10;
+    let Sy = (parseFloat(imgPosArr[1]) * 10) / 10;
+    let Sz = (parseFloat(imgPosArr[2]) * 10) / 10;
+
+    let Xx = (parseFloat(imgOriArr[0]) * 10) / 10;
+    let Xy = (parseFloat(imgOriArr[1]) * 10) / 10;
+    let Xz = (parseFloat(imgOriArr[2]) * 10) / 10;
+    let Yx = (parseFloat(imgOriArr[3]) * 10) / 10;
+    let Yy = (parseFloat(imgOriArr[4]) * 10) / 10;
+    let Yz = (parseFloat(imgOriArr[5]) * 10) / 10;
+
+    let Di = parseFloat((pixelSpaceArr[0]) * 10) / 10;
+    let Dj = parseFloat((pixelSpaceArr[1]) * 10) / 10;
+
+    let Px = [];
+    let Py = [];
+    let Pz = [];
+
+    for (let v = 0; v < Vi.length; v++) {
+        Px[v] = (Xx * Di * Vi[v]) + (Yx * Dj * Vj[v]) + Sx;
+        Py[v] = (Xy * Di * Vi[v]) + (Yy * Dj * Vj[v]) + Sy;
+        Pz[v] = (Xz * Di * Vi[v]) + (Yz * Dj * Vj[v]) + Sz;
+    }
+
+    drawDose(Px, Py, color);
+}
+
+function drawDose(Px, Py, color) {
+
     let canvas = document.getElementById('doseCanvas');
     let ctx = canvas.getContext('2d');
+
+    //draw path
+    ctx.beginPath();
+    for (let i = 0; i < Px.length; i++) {
+        ctx.lineTo(Px[i], Py[i]);
+    }
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.globalAlpha = 0.5;
+    ctx.stroke();
+    ctx.fill();
+
+}
+
+function resetDose(dose_value, checkVal_check, color) {
+
+    let canvas = document.getElementById('doseCanvas');
+    let ctx = canvas.getContext('2d');
+
 
     //draw path
     ctx.beginPath();
@@ -171,7 +195,9 @@ function drawDose(dose_value, checkVal_check, color) {
     ctx.globalAlpha = 0.5;
     ctx.stroke();
     ctx.fill();
-
+    canvas_obj[checkVal_check] = canvas;
 }
 
-export {doseFile, doseData, drawDose, getCheckValue, checkAndDraw}
+
+
+export {doseFile, doseData, drawDose, getCheckValue, checkAndDraw, resetDose}
