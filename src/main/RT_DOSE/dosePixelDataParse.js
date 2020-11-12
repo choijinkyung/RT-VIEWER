8dd6e7692d0c17dfcc937856e19d2100e5af790e
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-12 16:40:40 +0900

    fix pixel_data parsing

--- src/main/RT_DOSE/dosePixelDataParse.js
+++ src/main/RT_DOSE/dosePixelDataParse.js
@@ -9,37 +9,7 @@ function dose_pixel_Data_parse(image, dataSet) {
     Columns = parseFloat(dataSet.uint16('x00280011'));
     Number_of_Frames = parseFloat(image.data.string('x00280008'));
 
-    //초기화
-    let dose_grid = [];
-    for (let z = 0; z < Number_of_Frames; z++) {
-        dose_grid[z] = [];
-    }
-    for (let z = 0; z < Number_of_Frames; z++) {
-        for (let y = 0; y < Columns; y++) {
-            dose_grid[z][y] = [];
-        }
-    }
-
-    for (let z = 0; z < Number_of_Frames; z++) {
-        for (let y = 0; y < Columns; y++) {
-            for (let x = 0; x < Rows; x++) {
-                dose_grid[z][y][x] = [];
-            }
-        }
-    }
-    let count = 0;
-
-    for (let z = 0; z < Number_of_Frames; z++) {
-        for (let y = 0; y < Columns; y++) {
-            for (let x = 0; x < Rows; x++) {
-                dose_grid[z][y][x] = pixel_data[count];
-                count++;
-
-            }
-        }
-    }
-
-    gridScaling(image, dose_grid, Rows, Columns, Number_of_Frames);
+    gridScaling(image, pixel_data, Rows, Columns, Number_of_Frames);
 }
 
 
