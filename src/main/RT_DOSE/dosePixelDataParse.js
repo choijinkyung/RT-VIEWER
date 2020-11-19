111da117458ab34fff2bab4a201f6efa879d97c6
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-19 18:18:17 +0900

    import cornerstone

--- src/main/RT_DOSE/dosePixelDataParse.js
+++ src/main/RT_DOSE/dosePixelDataParse.js
@@ -1,9 +1,22 @@
 import {gridScaling} from "../Loader/firstLoader";
+import dcmjs from "dcmjs";
+import dicomParser from "dicom-parser";
+import * as cornerstone from "cornerstone-core";
+import * as cornerstoneTools from "cornerstone-tools";
+import * as cornerstoneMath from "cornerstone-math"
+import * as cornerstoneWadoImageLoader from "cornerstone-wado-image-loader"
 
+cornerstoneWadoImageLoader.external.cornerstone = cornerstone
+cornerstoneWadoImageLoader.external.dicomParser = dicomParser
+cornerstoneTools.external.cornerstone = cornerstone
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath
+cornerstoneTools.init();
 let Rows, Columns, Number_of_Frames;
+
 function dose_pixel_Data_parse(image, dataSet) {
     let pixelDataElement = dataSet.elements.x7fe00010;
-    let pixel_data = new Uint16Array(dataSet.byteArray.buffer, pixelDataElement.byteOffset, pixelDataElement.length / 4);
+
+    let pixel_data = new Uint16Array(dataSet.byteArray.buffer, pixelDataElement.byteOffset, pixelDataElement.length/4);
 
     Rows = parseFloat(dataSet.uint16('x00280010'));
     Columns = parseFloat(dataSet.uint16('x00280011'));
