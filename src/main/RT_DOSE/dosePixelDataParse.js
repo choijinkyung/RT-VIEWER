816b840a9b5a5129bdf9be0e49caeaf42a9c3154
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-20 16:44:59 +0900

    fix import pixel_data (success)

--- src/main/RT_DOSE/dosePixelDataParse.js
+++ src/main/RT_DOSE/dosePixelDataParse.js
@@ -14,16 +14,14 @@ cornerstoneTools.init();
 let Rows, Columns, Number_of_Frames;
 
 function dose_pixel_Data_parse(image, dataSet) {
-    let pixelDataElement = dataSet.elements.x7fe00010;
+        let pixelDataElement = dataSet.elements.x7fe00010;
+        let pixel_data = new Uint32Array(dataSet.byteArray.buffer.slice(pixelDataElement.dataOffset),0,pixelDataElement.length/4);
 
-    let pixel_data = new Uint16Array(dataSet.byteArray.buffer, pixelDataElement.byteOffset, pixelDataElement.length/4);
+        Rows = parseFloat(dataSet.uint16('x00280010'));
+        Columns = parseFloat(dataSet.uint16('x00280011'));
+        Number_of_Frames = parseFloat(image.data.string('x00280008'));
 
-    Rows = parseFloat(dataSet.uint16('x00280010'));
-    Columns = parseFloat(dataSet.uint16('x00280011'));
-    Number_of_Frames = parseFloat(image.data.string('x00280008'));
-
-    gridScaling(image, pixel_data, Rows, Columns, Number_of_Frames);
+        gridScaling(image, pixel_data, Rows, Columns, Number_of_Frames);
 }
-
 
 export {dose_pixel_Data_parse}
