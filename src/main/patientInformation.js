cffaf83ff11f459dc10284d9aaa9aaf86ef5ded0
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-24 16:54:23 +0900

    update UI elements

--- src/main/patientInformation.js
+++ src/main/patientInformation.js
@@ -3,18 +3,9 @@ import React from "react";
 function patientInformation(image) {
     try {
         // print the dicom information
-        document.getElementById('patient').textContent = 'Patient ID : ' + image.data.string('x00100020');
-        document.getElementById('topleft').textContent = 'Patient Name : ' + image.data.string('x00100010') + '\n' + 'Patient Sex : ' + image.data.string('x00100040');
-        document.getElementById('topright').textContent = 'Modality : ' + image.data.string('x00080060');
-        document.getElementById('modality').textContent = 'Modality : ' + image.data.string('x00080060');
-        document.getElementById('instanceUID').textContent = 'Instance UID : ' + image.data.string('x00080018');
-
-        /*
-        document.getElementById('studyUID').textContent = 'Study UID : ' + image.data.string('x0020000d');
-        document.getElementById('seriesUID').textContent = 'Series UID : ' + image.data.string('x0020000e');
-        document.getElementById('instanceUID').textContent = 'Instance UID : ' + image.data.string('x00080018');
-        document.getElementById('frameUID').textContent = 'Frame of Reference UID : ' + image.data.string('x00200052');
-         */
+        document.getElementById('patientName').textContent = 'Patient Name : ' + image.data.string('x00100010');
+        document.getElementById('patientID').textContent = 'Patient ID : ' + image.data.string('x00100020');
+        document.getElementById('gender').textContent = 'Gender : ' + image.data.string('x00100040');
 
     } catch (ex) {
         alert('parsing error');
