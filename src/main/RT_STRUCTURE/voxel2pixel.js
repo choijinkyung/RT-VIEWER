12e85f8062712f92c055edee5bf4e27e7537582c
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-24 16:54:05 +0900

    update pixel coords and RGBToHEX

--- src/main/RT_STRUCTURE/voxel2pixel.js
+++ src/main/RT_STRUCTURE/voxel2pixel.js
@@ -1,23 +1,16 @@
 import voxelCal from "./pixel2voxel";
 
-function isASCII(str) {
-    return /^[\x00-\x7F]*$/.test(str);
-}
-
 //Convert Parsed contour data to Array
 function pixelCal(image, struct) {
     let str = struct.split("\\");
     let vPx = []; //contour Data Px (voxel point x )
     let vPy = []; //contour Data Py (voxel point y )
-    let vPz = []; //contour Data Pz (voxel point z )
 
     for (let i = 0; i < str.length; i++) {
         if (i % 3 === 0) {
             vPx[i] = str[i];
         } else if (i % 3 === 1) {
             vPy[i] = str[i];
-        }else if (i % 3 === 2) {
-            vPz[i] = str[i];
         }
     }
 
@@ -40,5 +33,6 @@ function pixelCal(image, struct) {
     }
 
     return [pi, pj]
-};
+}
+
 export default pixelCal
