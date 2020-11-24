597e1356abd4c3ed65bc22322cf561ea9aa7589c
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-11-24 16:54:59 +0900

    update import data

--- src/main/RT_DOSE/importPixelData.js
+++ src/main/RT_DOSE/importPixelData.js
@@ -1,15 +1,15 @@
 import {gridScaling} from "../Loader/firstLoader";
 
 let Rows, Columns, Number_of_Frames;
-function dose_pixel_Data_parse(image, dataSet) {
+function dose_pixel_Data_parse(dose_image, dataSet) {
         let pixelDataElement = dataSet.elements.x7fe00010;
-        let pixel_data = new Uint32Array(dataSet.byteArray.buffer.slice(pixelDataElement.dataOffset),0,pixelDataElement.length/4);
+        let dose_pixel_data = new Uint32Array(dataSet.byteArray.buffer.slice(pixelDataElement.dataOffset),0,pixelDataElement.length/4);
 
         Rows = parseFloat(dataSet.uint16('x00280010'));
         Columns = parseFloat(dataSet.uint16('x00280011'));
-        Number_of_Frames = parseFloat(image.data.string('x00280008'));
+        Number_of_Frames = parseFloat(dose_image.data.string('x00280008'));
 
-        gridScaling(image, pixel_data, Rows, Columns, Number_of_Frames);
+        gridScaling(dose_image, dose_pixel_data, Rows, Columns, Number_of_Frames);
 }
 
 export {dose_pixel_Data_parse}
