475f4600927294cb5dde1be244d45af9ae10d855
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-12-02 13:51:01 +0900

    API documentation

--- src/main/mouseControl.js
+++ src/main/mouseControl.js
@@ -5,6 +5,29 @@ import {MapInteractionCSS} from "react-map-interaction";
  * @class
  * @description
  * Performs CT Image, DOSE, and RT STRUCTURE simultaneously with Zoom in, out, and pan.
+ * <br>This is open source - react-map-interaction
+ *
+ * To Install
+ * <br> -> npm install react-map-interaction
+ *
+ * To apply
+ *  1. import Controlled from "./mouseControl";
+ *  2. Where there is a tag you want, Put <Controled// in place of the tag.
+ *  3. Then, put the tag in the Controlled class.
+ *
+ * @example
+ * //How to use?
+ * //In Controlled Class
+ *     render() {
+        return (
+            <MapInteractionCSS
+                value={this.state.value}
+                onChange={(value) => this.setState({value})}>
+                // Put the tag you want to apply here.
+            </MapInteractionCSS>
+        );
+    }
+ *
  * @author Choi jin kyung
  */
 class Controlled extends React.Component {
@@ -19,8 +42,6 @@ class Controlled extends React.Component {
     }
 
     render() {
-        const {scale, translation} = this.state;
-
         return (
             <MapInteractionCSS
                 value={this.state.value}
