1d62222c5afcf15c56b430ee6ab735eae43b4e9d
j_iky <44565579+choijinkyung@users.noreply.github.com>
2020-12-02 09:32:34 +0900

    API Documentation and Split into two files

--- src/main/RT_STRUCTURE/drawROI.js
+++ src/main/RT_STRUCTURE/drawROI.js
@@ -1,402 +1,21 @@
-import $ from "jquery";
-import dicomParser from "dicom-parser";
-import {ROI_addCheckbox,ROI_checkEvent} from "./ROIcheckbox";
 import pixelCal from "./voxel2pixel";
 import fullColorHex from "./rgbToHex.js";
-
-function isASCII(str) {
-    return /^[\x00-\x7F]*$/.test(str);
-}
-
-let dataSet;
-
-function structFile(file) {
-    // clear any data currently being displayed as we parse this next file
-    //document.getElementById('rtstruct').innerHTML = '';
-    //document.getElementById('rtstruct3').innerHTML = '';
-
-    $('#status').removeClass('alert-warning alert-success alert-danger').addClass('alert-info');
-    $('#warnings').empty();
-    //document.getElementById('statusText2').innerHTML = 'Status: Loading file, please wait..';
-
-    let reader = new FileReader();
-    reader.onload = function (file) {
-        let arrayBuffer = reader.result;
-        // Here we have the file data as an ArrayBuffer.  dicomParser requires as input a
-        // Uint8Array so we create that here
-        let byteArray = new Uint8Array(arrayBuffer);
-
-        // set a short timeout to do the parse so the DOM has time to update itself with the above message
-        setTimeout(function () {
-            // Invoke the paresDicom function and get back a DataSet object with the contents
-            try {
-                dataSet = dicomParser.parseDicom(byteArray);
-
-                let output1 = [];
-                let output3 = [];
-
-                ROIList(dataSet);
-                contour(dataSet, output1, output3);
-                roiJson(roi_List);
-                contourJson(contourList);
-
-                ROI_checkEvent();
-
-            } catch (err) {
-                if (err.output1 || err.output3) {
-
-                } else if (err.dataSet) {
-                    var output1 = [];
-                    var output3 = [];
-
-                    ROIList(err.dataSet);
-                    contour(err.dataSet, output1, output3);
-                }
-            }
-        }, 10);
-    };
-    reader.readAsArrayBuffer(file);
-
-    return dataSet;
-}
-
-let roi_List = [];
-
-function ROIList(dataSet) {
-    try {
-        for (let propertyName in dataSet.elements) {
-
-            let element = dataSet.elements[propertyName];
-            //show ROI List
-            if (element.tag === 'x30060020' || (element.tag === 'x30060022') || element.tag === 'x30060026') {
-                let text = element.tag;
-                if (element.hadUndefinedLength) {
-                    text += " <strong>(-1)</strong>";
-                }
-                text += "; ";
-                if (element.vr) {
-                    text += " VR=" + element.vr + "; ";
-                }
-                let color = 'black';
-
-                if (element.items) { //item들을 표시
-                    element.items.forEach(function (item) {
-                        // each item contains its own data set so we iterate over the items and recursively call this function
-                        ROIList(item.dataSet);
-                    });
-                } else if (element.fragments) {
-                    // each item contains its own data set so we iterate over the items and recursively call this function
-                    let itemNumber = 0;
-                    element.fragments.forEach(function (fragment) {
-                        let basicOffset;
-                        if (element.basicOffsetTable) {
-                            basicOffset = element.basicOffsetTable[itemNumber];
-                        }
-                        let str = '<li>Fragment #' + itemNumber++ + ' offset = ' + fragment.offset;
-                        str += '(' + basicOffset + ')';
-                        str += '; length = ' + fragment.length + '</li>';
-                    });
-                } else {
-                    if (element.length === 2) {
-                        text += " (" + dataSet.uint16(propertyName) + ")";
-
-                    } else if (element.length === 4) {
-                        text += " (" + dataSet.uint32(propertyName) + ")";
-                    }
-                    //대부분은 문자열이지만 그렇지 않은 것들을 확인해서 표시하는 것을 위함
-                    let str = dataSet.string(propertyName);
-                    let stringIsAscii = isASCII(str);
-
-                    if (stringIsAscii) {
-                        // 정의되지 않은 경우 아무것도 넣지 않음
-                        if (str !== undefined) {
-                            text += '"' + str + '"';
-                        }
-                    } else {
-                        if (element.length !== 2 && element.length !== 4) {
-                            color = '#C8C8C8';
-                            // If it is some other length and we have no string
-                            text += "<i>binary data</i>";
-                        }
-                    }
-                    if (element.length === 0) {
-                        color = '#C8C8C8';
-                    }
-                    roi_List.push(element.tag, dataSet.string(propertyName));
-                }
-            }
-        }
-    } catch (err) {
-        let ex = {
-            exception: err
-        }
-        throw ex;
-    }
-}
-
-let contourList = [];
-
-function contour(dataSet, output1, output3) {
-    try {
-        for (let propertyName in dataSet.elements) {
-            let element = dataSet.elements[propertyName];
-            //show contour data list
-
-            if (element.tag === 'x30060039' || element.tag === 'x3006002a' || element.tag === 'x30060040' || element.tag === 'x30060050' || element.tag === 'x30060016' || element.tag === 'x00081155' || element.tag === 'x30060084') {
-                let text = element.tag;
-
-                if (element.hadUndefinedLength) {
-                    text += " <strong>(-1)</strong>";
-                }
-                text += "; ";
-                if (element.vr) {
-                    text += " VR=" + element.vr + "; ";
-                }
-                let color = 'black';
-
-                // Here we check for Sequence items and iterate over them if present.
-                // items will not be set in the element object for elements that don't have SQ VR type.
-                //  Note that implicit little endian sequences will are currently not parsed.
-                if (element.items) { //item들을 표시
-                    // itemNumber = 0;
-                    element.items.forEach(function (item) {
-                        // each item contains its own data set so we iterate over the items and recursively call this function
-                        //   output1.push('<li class = "item">Item #' + itemNumber++ + ' ' + item.tag + '</li>')
-                        //   output1.push('<ul class = "data">');
-                        contour(item.dataSet, output1, output3);
-
-                        // output1.push('</ul>');
-                    });
-                } else if (element.fragments) {
-                    // each item contains its own data set so we iterate over the items and recursively call this function
-                    let itemNumber = 0;
-                    element.fragments.forEach(function (fragment) {
-                        let basicOffset;
-                        if (element.basicOffsetTable) {
-                            basicOffset = element.basicOffsetTable[itemNumber];
-                        }
-                        let str = '<li>Fragment #' + itemNumber++ + ' offset = ' + fragment.offset;
-                        str += '(' + basicOffset + ')';
-                        str += '; length = ' + fragment.length + '</li>';
-                    });
-                } else {
-                    if (element.length === 2) {
-                        text += " (" + dataSet.uint16(propertyName) + ")";
-
-                    } else if (element.length === 4) {
-                        text += " (" + dataSet.uint32(propertyName) + ")";
-                    }
-                    //대부분은 문자열이지만 그렇지 않은 것들을 확인해서 표시하는 것을 위함
-                    let str = dataSet.string(propertyName);
-                    let stringIsAscii = isASCII(str);
-
-                    if (stringIsAscii) {
-                        // 정의되지 않은 경우 아무것도 넣지 않음
-                        if (str !== undefined) {
-                            text += '"' + str + '"';
-                        }
-                    } else {
-                        if (element.length !== 2 && element.length !== 4) {
-                            color = '#C8C8C8';
-                            // If it is some other length and we have no string
-                            text += "<i>binary data</i>";
-                        }
-                    }
-                    if (element.length === 0) {
-                        color = '#C8C8C8';
-                    }
-                    //   output3.push('<li style="color:' + color + ';">' + text + '</li>');
-                    // output1.push('<li style="color:' + color + ';">' + text + '</li>');
-                    // finally we add the string to our  array
-                    contourList.push(element.tag, dataSet.string(propertyName));
-                }
-            }
-        }
-    } catch (err) {
-        let ex = {
-            exception: err,
-            output3: output3
-        }
-        throw ex;
-    }
-    return contourList;
-}
-
-function roiJson(roi_List) {
-    let ROI_Number = [];
-    let ROI_Name = [];
-
-    for (let i = 0; i < roi_List.length; i++) {
-        if (roi_List[i] === 'x30060022') {
-            ROI_Number[i] = roi_List[i + 1];
-        } else if (roi_List[i] === 'x30060026') {
-            ROI_Name[i] = roi_List[i + 1];
-        }
-    }
-
-    //remove undefined in array
-    ROI_Number = ROI_Number.filter(function (item) {
-        return item !== null && item !== undefined && item !== "";
-    })
-    ROI_Name = ROI_Name.filter(function (item) {
-        return item !== null && item !== undefined && item !== "";
-    })
-
-    //parsing to JSON
-    $(function () {
-        let ROI_LIST_Array = [];
-
-        for (let i = 0; i < ROI_Name.length; i++) {
-            let ROI_object = {};
-            ROI_object.x30060022 = ROI_Number[i];
-            ROI_object.x30060026 = ROI_Name[i];
-
-            ROI_LIST_Array.push(ROI_object);
-        }
-        ROI_addCheckbox(ROI_LIST_Array);
-    });
-
-}
-
-let contour_data_Array = [];
-
-function contourJson(contourList) {
-    let Referenced_Instance_UID = [];
-    let contour_data = [];
-    let Referenced_ROI_Number = [];
-    let ROI_display_color = [];
-
-    for (let i = 0; i < contourList.length; i++) {
-        let j = 0;
-        if (contourList[i + 2] === 'x00081155') {
-            Referenced_Instance_UID[i] = contourList[i + 3];
-            contour_data[i] = contourList[i + 5];
-            if (contourList[i + 6] === 'x30060084') {
-                Referenced_ROI_Number[i] = contourList[i + 7];
-            } else if (contourList[i + 6] === 'x00081155') {
-                for (j = i + 6; j < contourList.length; j++) {
-                    if (contourList[j + 2] === 'x30060084') {
-                        Referenced_ROI_Number[i] = contourList[j + 3];
-                        break;
-                    }
-                }
-            }
-            if (contourList[i] === 'x3006002a') {
-                ROI_display_color[i] = contourList[i + 1];
-            } else if (contourList[i] === 'x30060050') {
-                for (j = i; j > 0; j--) {
-                    if (contourList[j] === 'x3006002a') {
-                        ROI_display_color[i] = contourList[j + 1];
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    ROI_display_color = ROI_display_color.filter(function (item) {
-        return item !== null && item !== undefined && item !== "";
-    });
-    Referenced_Instance_UID = Referenced_Instance_UID.filter(function (item) {
-        return item !== null && item !== undefined && item !== "";
-    });
-    contour_data = contour_data.filter(function (item) {
-        return item !== null && item !== undefined && item !== "";
-    });
-    Referenced_ROI_Number = Referenced_ROI_Number.filter(function (item) {
-        return item !== null && item !== undefined && item !== "";
-    });
-
-    //parsing to JSON
-    $(function () {
-        for (let i = 0; i < contourList.length; i++) {
-            let contour_object = {};
-            contour_object.x30060084 = Referenced_ROI_Number[i];
-            contour_object.x00081155 = Referenced_Instance_UID[i];
-            contour_object.x30060050 = contour_data[i];
-            contour_object.x3006002a = ROI_display_color[i];
-
-            contour_data_Array.push(contour_object);
-        }
-    });
-}
-
-let information = {
-    ROIs: []
-}
-let struct, color;
-
-let checkVal_send = information.ROIs;
-
-function sendDrawImage(image) {
-    let Instance_UID = image.data.string('x00080018');
-
-    for(let j=0;j<checkVal_send.length;j++){
-        for (let i = 0; i < contour_data_Array.length; i++) {
-            if (contour_data_Array[i]['x30060084'] === checkVal_send[j]) {
-                if (contour_data_Array[i]['x00081155'] === Instance_UID) {
-                    struct = contour_data_Array[i]['x30060050'];
-                    color = contour_data_Array[i]['x3006002a'];
-
-                    draw(image, struct, color);
-                }
-            }
-        }
-    }
-}
-
-let img;
-function getCTImage(image){
-    img = image;
-    return img;
-}
-
-function checkAndDraw(checkVal_check) {
-    let Instance_UID = img.data.string('x00080018');
-    for (let i = 0; i < contour_data_Array.length; i++) {
-        if (contour_data_Array[i]['x30060084'] === checkVal_check) {
-            if (contour_data_Array[i]['x00081155'] === Instance_UID) {
-                struct = contour_data_Array[i]['x30060050'];
-                color = contour_data_Array[i]['x3006002a'];
-
-                draw(img, struct, color);
-            }
-        }
-    }
-}
-
-function checkAndReset(checkVal_check) {
-    let Instance_UID = img.data.string('x00080018');
-    for (let i = 0; i < contour_data_Array.length; i++) {
-        if (contour_data_Array[i]['x30060084'] === checkVal_check) {
-            if (contour_data_Array[i]['x00081155'] === Instance_UID) {
-                struct = contour_data_Array[i]['x30060050'];
-                color = contour_data_Array[i]['x3006002a'];
-
-            }
-        }
-    }
-
-}
-/*Function*/
-function addROIset(evt) {
-    let checkVal_check;
-    if (evt.target.checked === true) {
-        information.ROIs.push(evt.target.value);
-        checkVal_check = evt.target.value;
-
-        checkAndDraw(checkVal_check);
-    }else {
-        let index = information.ROIs.indexOf(evt.target.value);
-        if (index !== -1){
-            information.ROIs.splice(index, 1);
-        }
-        checkVal_check = evt.target.value;
-        checkAndReset(checkVal_check);
-    }
-}
-
-function draw(image,struct,color) {
+/**
+ * @function drawROI
+ * @param {object} image -> CT image corresponding to the current z coordinate
+ * @param {string} struct -> Corresponding to current CT image, contour data of checked ROI
+ * @param {string} color -> contouring color
+ * @description
+ * This function deals with
+ * 1. Convert to CT->canvas coordinates through pixelCal function.
+ * 2. Draw the corresponding ROI.
+ * 3. Function call
+ * <br> 1)name : pixelCal
+ * <br> param : image, struct
+ * <br> 2)name : fullColorHex
+ * <br> param : color
+ */
+function drawROI(image, struct, color) {
     let px = pixelCal(image, struct);
     let pi = px[0];
     let pj = px[1];
@@ -410,7 +29,6 @@ function draw(image,struct,color) {
     for (let i = 1; i <= pi.length * 3; i++) {
         if (i % 3 === 0) {
             ctx.lineTo(pi[i], pj[i + 1]);
-
         }
     }
     ctx.closePath();
@@ -419,19 +37,24 @@ function draw(image,struct,color) {
     ctx.stroke();
     ctx.fill();
     ctx.restore();
-
 }
 
+/**
+ * @function reset
+ * @description
+ * This function deals with
+ * 1. Erase the canvas.
+ * 2. Control the canvas from overlapping during mouse wheel events
+ */
 function reset() {
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
 
     let canvas2 = document.getElementById('doseCanvas');
     let ctx2 = canvas2.getContext("2d");
-    ctx.clearRect(0,0,512,512);
-    ctx2.clearRect(0,0,512,512);
+    ctx.clearRect(0, 0, 512, 512);
+    ctx2.clearRect(0, 0, 512, 512);
 }
 
 
-
-export {structFile, draw, reset , sendDrawImage, getCTImage , addROIset}
+export {drawROI, reset}
